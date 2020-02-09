33d0bb2b9a5549dbfa75d0c526e4c3d079a98b5d
Ines <s1730121@ed.ac.uk>
2020-02-09 03:51:02 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function responsiveTopnav() {
+  var x = document.getElementById("topnavbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
